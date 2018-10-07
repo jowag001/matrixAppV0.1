e66b334d159f7ef77c8b3bec70cb9c2e03ca1ceb
jowag001 <johann_wagner@outlook.com>
2018-10-07 16:16:03 +0200

    circleindicator done

--- app/src/main/java/com/example/ivany4/matrixapp/MainActivity.java
+++ app/src/main/java/com/example/ivany4/matrixapp/MainActivity.java
@@ -3,10 +3,12 @@ package com.example.ivany4.matrixapp;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import me.relex.circleindicator.CircleIndicator;
 
 public class MainActivity extends AppCompatActivity {
     PagerAdapter collectionPagerAdapter;
     private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,11 @@ public class MainActivity extends AppCompatActivity {
         collectionPagerAdapter = new PagerAdapter(getSupportFragmentManager());
         viewPager = (ViewPager) findViewById(R.id.pager);
         viewPager.setAdapter(collectionPagerAdapter);
+
+        CircleIndicator indicator = (CircleIndicator) findViewById(R.id.indicator);
+        indicator.setViewPager(viewPager);
+
+
 
 
     }
